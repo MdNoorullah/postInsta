0414141a62e8b446f99a00460aa26d46a2a3a572
Md Noorullah <77206298+MdNoorullah@users.noreply.github.com>
2022-06-11 05:13:35 +0530

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
-const JWT_SECRET = require('../config/keys')
+const {JWT_SECRET} = require('../config/keys')
 const mongoose= require('mongoose')
 const User=mongoose.model("User")
 module.exports=(req,res,next)=>{

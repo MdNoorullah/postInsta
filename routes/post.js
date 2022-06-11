8d463a90049dad40f3b414fde7e9278e9ccacfa9
Md Noorullah <77206298+MdNoorullah@users.noreply.github.com>
2022-06-11 05:37:48 +0530

    made post to post request type

--- routes/post.js
+++ routes/post.js
@@ -20,7 +20,7 @@ router.get('/allpost',requireLogin,(req,res)=>{
     
 })
 
-router.get('/getsubpost',requireLogin,(req,res)=>{
+router.post('/getsubpost',requireLogin,(req,res)=>{
 
     // if postedBy in following
     Post.find({postedBy:{$in:req.user.following}})//search in following array
